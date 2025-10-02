0a6accebdb318f1192db1beb0e3d3f1016e7bc50
Navendu Gopan <navendugopan23@gmail.com>
2025-10-02 22:22:22 +0530

    registration opened

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ export default function Footer() {
             <img src="/assets/youtube.svg" alt="YouTube" className="h-12 w-12" loading="lazy" />
           </a>
         </div>
-        <a href="#tickets" className="bg-red-700 text-white px-4 py-2 rounded font-aderos reg-btn">REGISTER NOW</a>
+        <a href="https://forms.gle/XKA4gsYefJPyRAYB6" className="bg-red-700 text-white px-4 py-2 rounded font-aderos reg-btn">REGISTER NOW</a>
       </div>
     </footer>
   )
